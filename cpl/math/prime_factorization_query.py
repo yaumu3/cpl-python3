41edf40050a8e67b5bfe1d24e7d1fffc0af59a89
yaumu3 <yumu@me.com>
2021-03-06 10:27:39 +0900

    Add log(n) prime factorization without verification

---
+++ cpl/math/prime_factorization_query.py
@@ -0,0 +1,42 @@
+from typing import List
+
+
+class SmallestPrimeFactors:
+    """High speed prime factorizaton using smallest prime factors
+    Precompute the smallest prime factor for each integer less than or equal to `n`
+    by Eratosthenes sieve; tiem complexity O(n*log(log(n))).
+    Then, factorize integer `x` with time complexity O(log(x))
+    Based on `https://atcoder.jp/contests/abc177/editorial/82`.
+
+    Args:
+        n: Max number to factorize or judge primality.
+
+    Attributes:
+        spf: List of the smallest prime factor whose index is the corresponding integer.
+    """
+
+    def __init__(self, n: int) -> None:
+        self.spf = list(range(n + 1))
+        self.spf[0] = -1
+        self.spf[1] = -1
+        for i in range(2, int(n ** 0.5) + 1):
+            if self.spf[i] == i:
+                for j in range(i * i, n + 1, i):
+                    if self.spf[j] == j:
+                        self.spf[j] = i
+
+    def is_prime(self, x: int) -> bool:
+        assert x < len(self.spf)
+        return self.spf[x] == x
+
+    def __traverse(self, x: int) -> List[int]:
+        if self.spf[x] == x:
+            return [x]
+        nxt = x // self.spf[x]
+        return self.__traverse(nxt) + [self.spf[x]]
+
+    def factor(self, x: int) -> List[int]:
+        assert x < len(self.spf)
+        if x < 2:
+            return []
+        return self.__traverse(x)
